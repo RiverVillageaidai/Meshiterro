a549e8044ce0d1cd4aaced8532c4e7081e069edf
EC2 Default User <ec2-user@ip-172-31-16-107.ap-northeast-1.compute.internal>
2024-10-25 10:03:58 +0000

    [Add]PostImagesコントローラー new index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-
-  devise_for :users
   root 'homes#top'
+  devise_for :users
+  # コントローラの同じ名前のアクションに紐づく形で、ルーティングを自動生成
+  resources :post_images, only: [:new, :index, :show]
+  
+  
   get 'homes/about' => 'homes#about' , as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
