c67d111eb83e5a95d8c2b121948b4ab12b8b62eb
EC2 Default User <ec2-user@ip-172-31-16-107.ap-northeast-1.compute.internal>
2024-10-25 09:38:38 +0000

    [Add]アソシエーションuser  post_image

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,8 @@
 class PostImage < ApplicationRecord
   
   has_one_attached :image
+  
+  # belongs_to    アソシエーション
+  belongs_to :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
 # :validatable（email のフォーマットなどのバリデーション）
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  # has_many アソシエーション　dependent: :destroy 1:Nの1側が削除されたとき、N側を全て削除する
+  has_many :post_images, dependent: :destroy
+  
 end
