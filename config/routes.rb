05d58e86214a779894ae38f4dd6560825c315aeb
EC2 Default User <ec2-user@ip-172-31-16-107.ap-northeast-1.compute.internal>
2024-10-26 09:49:40 +0000

    [Add]usersコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
   root 'homes#top'
   devise_for :users
+  get 'homes/about' => 'homes#about' , as: 'about'
   # コントローラの同じ名前のアクションに紐づく形で、ルーティングを自動生成
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  
+  resources :users, only: [:show, :edit]
 
-
-  get 'homes/about' => 'homes#about' , as: 'about'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
