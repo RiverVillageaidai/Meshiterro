daa45a75965aa8c8e552a8212b439be5ad261983
EC2 Default User <ec2-user@ip-172-31-16-107.ap-northeast-1.compute.internal>
2024-10-26 05:39:44 +0000

    [復元]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に
   # configure_permitted_parametersメソッドが実行されます。
   before_action :configure_permitted_parameters,if: :devise_controller?
-  
+
   # after_sign_in_path_forはDeviseが用意しているメソッド サインイン後にどこに遷移するかを設定している
   # 引数resourceはログインを実行したモデルのデータ、今回の場合はログインしたUserのインスタンスが格納されています。
   def after_sign_in_path_for(resource)
-    post_images_pa
+    post_images_path
   end
    # after_sign_out_path_forはDeviseが用意しているメソッド サインアウト後にどこに遷移するかを設定している
   def after_sign_out_path_for(resource)
