9f996361512d9c0feb5a389bc7b455dcdebdcdf3
EC2 Default User <ec2-user@ip-172-31-16-107.ap-northeast-1.compute.internal>
2024-10-26 02:53:53 +0000

    [Add]投稿削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   # コントローラの同じ名前のアクションに紐づく形で、ルーティングを自動生成
-  resources :post_images, only: [:new, :create, :index, :show]
-  
-  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+
   get 'homes/about' => 'homes#about' , as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
