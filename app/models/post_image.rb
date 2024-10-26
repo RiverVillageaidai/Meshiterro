cba52cefeeaf29d1b9e50ca489a77351a45015a6
EC2 Default User <ec2-user@ip-172-31-16-107.ap-northeast-1.compute.internal>
2024-10-26 02:21:09 +0000

    [Add]view post_images/show

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -7,7 +7,9 @@ class PostImage < ApplicationRecord
   
   # 画像表示用のメソッド
   def get_image
+    # unless ifと逆
     unless image.attached?
+      # `Rails.root.join`は、Railsアプリケーションでファイルパスを生成するためのメソッド
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg',content_type: 'image/jpeg' )
     end
