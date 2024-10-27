3a5c8d5b3a858d4066f90984962e986fe4cb624c
EC2 Default User <ec2-user@ip-172-31-16-107.ap-northeast-1.compute.internal>
2024-10-27 04:40:58 +0000

    [Add]プロフィール編集画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
-    
     @post_images = @user.post_images
   end
 
   def edit
+    @user = User.find(current_user.id)
   end
 end
