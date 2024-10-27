c124109579d4b23ca2c719f94e8e3ad8237c1665
EC2 Default User <ec2-user@ip-172-31-16-107.ap-northeast-1.compute.internal>
2024-10-27 05:20:34 +0000

    [Update]コメント

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,20 @@
 class UsersController < ApplicationController
+  # プロフィール詳細
   def show
     @user = User.find(current_user.id)
     @post_images = @user.post_images
   end
 
+  #プロフィール編集
   def edit
     @user = User.find(current_user.id)
   end
-  
+
+  # プロフィール更新
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id) 
+    redirect_to user_path(@user.id)
   end
 
 
@@ -19,6 +22,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
-  end  
-  
+  end
+
 end
