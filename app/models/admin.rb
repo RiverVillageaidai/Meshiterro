58a27fd51cddc9a48838fa282bce09f4145ab07e
EC2 Default User <ec2-user@ip-172-31-33-82.eu-north-1.compute.internal>
2024-12-02 12:40:41 +0000

    [START]管理者機能

---
+++ app/models/admin.rb
@@ -0,0 +1,9 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Admin側でemailを使ってログイン
+  devise authentication_keys: [:email]
+end
